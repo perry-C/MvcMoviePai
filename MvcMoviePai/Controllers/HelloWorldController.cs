1022ff41aee7e4258b1e5ac74341f26c4bd40f34
Perry Chen <2558544545qwerty@gmail.com>
2024-08-09 15:48:17 +0100

    Add project files.

---
+++ MvcMoviePai/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Encodings.Web;
+using System.Xml.Linq;
+
+namespace MvcMoviePai.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+
+        return View();
+    }
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+
+    /* Every public method in a controller is callable as an HTTP endpoint.
+     * In the sample above, both methods return a string. Note the comments preceding each method.*/
+}
---
+++ MvcMoviePai/Data/MvcMoviePaiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMoviePai.Models;
+
+namespace MvcMoviePai.Data
+{
+    public class MvcMoviePaiContext : DbContext
+    {
+        public MvcMoviePaiContext (DbContextOptions<MvcMoviePaiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMoviePai.Models.Movie> Movie { get; set; } = default!;
+    }
+}
